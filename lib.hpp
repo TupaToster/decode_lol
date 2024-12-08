fd24dfbc9b719d2afa7a8803b2ab4659756c9856
TupaToster <sidorchuk.me@phystech.edu>
2024-12-08 07:12:48 +0300

    a clean slate; a clean start; a clean life

--- lib.hpp
+++ lib.hpp
@@ -2,3 +2,7 @@
 #include <iostream>
 #include <vector>
 
+typedef u_int8_t Byte_t;
+typedef u_int32_t Reg_t;
+
+
--- main.cpp
+++ main.cpp
@@ -3,6 +3,4 @@
 int main (int argc, char* argv[]) {
 
     std::cout << "Helo, i will be your decoder + cpu + something else today\n";
-
-    
 }
